c1f88b2e320e1d380100734d76f36fe4db51d4fe
asyylz <49663592+asyylz@users.noreply.github.com>
2024-02-10 21:29:50 +0000

    img sources  added to json file and functionality added

--- 6-FlowerShopping/app.js
+++ 6-FlowerShopping/app.js
@@ -1,10 +1,18 @@
 "use strict";
+/* ------------------------ JSON ------------------------ */
 
 fetch("products.json") // returns a promise so we can use then()
   .then(function (response) {
     return response.json(); // parses JSON response into native JavaScript and also returns a promise so we can use then() again
   })
   .then(function (data) {
+    for (let i = 1; i < data.length + 1; i++) {
+      const imgURL = data[i - 1].image;
+      const imgElement = document.getElementById(`${i}`);
+      if (imgElement) {
+        imgElement.src = imgURL;
+      }
+    }
     // saves  data to local storage
     localStorage.setItem("products", JSON.stringify(data));
     if (!localStorage.getItem("cart")) {
@@ -16,25 +24,34 @@ fetch("products.json") // returns a promise so we can use then()
 const products = JSON.parse(localStorage.getItem("products")) || [];
 const cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-//let selectedProduct;
+/* -------------------- addCartBtnId -------------------- */
 
-/* ----------------- Adding the product in teh cart ---------------- */
-//const sectionProducts = document.querySelector("section.py-5");
+/* ----------------- Adding product eventlistener option 1 ---------------- */
+// const sectionProducts = document.querySelector("section.py-2");
+// sectionProducts.addEventListener("click", function (e) {
+//   console.log(e.target)
+//   if (e.target && e.target.classList.contains("btnAdd")) {
+//     const card = e.target.closest(".card");
+//     const img = card.querySelector(".card-img-top");
+//     const productId = parseInt(img.getAttribute("id"));
+//     addProductToCart(productId);
+//   }
+// });
+/* ----------------- Adding product eventlistener option 2 ---------------- */
 const btnsAddToCart = document.querySelectorAll(".btnAdd");
-
-console.log(btnsAddToCart);
 btnsAddToCart.forEach((btn) => {
   btn.addEventListener("click", function (e) {
     e.preventDefault();
     const productId = parseInt(this.getAttribute("id"));
-
     addProductToCart(productId);
   });
 });
 
+/* ------------------- Top Cart Total ------------------- */
 let cartTotal = document.querySelector("span.badge");
 cartTotal.textContent = 0;
 
+/* ------------ Product to cart funtionality ----------- */
 function addProductToCart(productId) {
   const selectedProduct = products.find((product) => product.id === productId);
   if (!selectedProduct) {
@@ -49,7 +66,7 @@ function addProductToCart(productId) {
     } else if (existingProduct.stockQuantity > existingProduct.saleQuantity) {
       existingProduct.saleQuantity++;
     } else {
-      console.log(`${existingProduct.name} no more available in stock`);
+      alert(`${existingProduct.name} no more available in stock`);
     }
     localStorage.setItem("cart", JSON.stringify(cart));
 
